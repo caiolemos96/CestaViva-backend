342608d625f15338ee418b5b9229ab2f406b422b
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-09-13 23:18:05 -0300

    mera formatação visual. Inclusão de linhas vazias para facilitar a leitura do código.

--- src/routes/entidadesRoutes.js
+++ src/routes/entidadesRoutes.js
@@ -7,13 +7,16 @@ const routes = express.Router();
 
 routes.get("/entidades", EntidadeController.listarEntidades);
 routes.get("/entidades/:id", EntidadeController.listarEntidadePorId);
+
 routes.post("/entidades", EntidadeController.cadastrarEntidade);
 routes.post(
   "/entidades/cadastrar/usuario",
   checkToken,
   UserController.cadastrarUsuario
 );
+
 routes.put("/entidades/:id", EntidadeController.atualizarEntidadePorId);
+
 routes.delete("/entidades/:id", EntidadeController.excluirEntidadePorId);
 
 export default routes;

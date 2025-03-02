ea73212fb5c212b90b20c24edd7c0fdd01a5746c
caio <caio96.lemos@gmail.com>
2025-03-02 15:35:22 -0300

    feat: Adiciona rotas e controlador para upload de imagens no Imgur

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import auth from "./authRoutes.js"; //importando as rotas de autenticação
 import cesta from "./cestasRoutes.js";
 import produtos from "./produtoRoutes.js";
 import mail from "./mailRoutes.js"
+import imgur from "./imgurRoutes.js"
 
 //funcao para agrupar todas as rotas que vamos receber
 
@@ -14,7 +15,7 @@ const routes = (app) => {
 
   //incluir as rotas
   //middleware das rotas que estao vindo de entidadesRoutes, passando tudo pra routes gerenciar
-  app.use(express.json(), entidades, auth, cesta, produtos, mail);
+  app.use(express.json(), entidades, auth, cesta, produtos, mail, imgur);
 };
 
 export default routes;
